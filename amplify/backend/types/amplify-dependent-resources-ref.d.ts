b380a30f7725dab6ffd3581ffd03e65a0e5988b1
Alec Loveland <ajlovelandwater@gmail.com>
2023-05-15 22:20:15 -0400

    first commit react app created

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "chatapp29e84d6b": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
